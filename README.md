dc8ab9dbf99df070d203dff0634bdb7d4bfff1d2
Nicolas LEBRETON <>
2016-06-01 14:42:00 +0400

    Base du site de la Ligue Pokémon Réunionnaise

---
+++ README.md
@@ -0,0 +1,4 @@
+pokerun
+=======
+
+A Symfony project created on June 1, 2016, 9:30 am.
---
+++ src/MainBundle/Controller/MainController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('MainBundle:Main:index.html.twig', array(
+                // ...
+            ));    }
+
+}
